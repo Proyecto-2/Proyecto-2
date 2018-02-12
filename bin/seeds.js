5dda7cfac3341107cd4d15dfb4cf5a1b489d7220
Jose Sanchez <joseansanher@gmail.com>
2018-02-12 11:57:45 +0100

    db created

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,18 +1,32 @@
-const mongoose = require('mongoose');
-const {dbURL} = require('../config');
-const Product = require('../models/Product');
+const mongoose = require("mongoose");
+const Auth = require("../models/User");
 
-mongoose.connect(dbURL)
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-const products = [
+mongoose.connect("mongodb://localhost/project2").then(() => console.log("Conectado"));
+
+const salt = bcrypt.genSaltSync(bcryptSalt);
+
+const auth = [
     {
-        name: 'Desnada',
-        price: 9.99,
-        description: 'Para cuidar la linea'
-    },
-    {
-        name: 'Desnada',
-        price: 9.99,
-        description: 'Para cuidar la linea'
+        name: String,
+        username: 'John',
+        password: bcrypt.hashSync("123", salt),
+        mail: String
     }
-];
+]
+
+Auth.collection.drop();
+
+auth.forEach(c => {
+    let au = new Auth(c);
+    au.save((err, auth) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`User saved ${auth.username}`);
+        mongoose.disconnect();
+    })
+});
